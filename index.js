aea469dbb61588508efa8151e1cd03370d2646de
Rakib <spyer.rakib@gmail.com>
2026-02-20 09:21:45 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running"));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
